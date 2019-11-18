70632917e330f10fe34a86704bd90fd9bfeb052c
ayusha-pradhan <39608243+ayusha-pradhan@users.noreply.github.com>
2019-11-18 15:11:22 +0545

    Update f2.js

--- f2.js
+++ f2.js
@@ -2,13 +2,6 @@
 
 var array = [ { name: "ayu", bar: 2}, { name: "alex", bar: 4}, { name: "ana", bar: 6} ];
 
-function getFields(input, field) {
-    var output = [];
-    for (var i=0; i < input.length ; ++i)
-        output.push(input[i][field]);
-    return output;
-}
-
 let result = array.map(a => (a.name));
  console.log(array);
 console.log(result);
