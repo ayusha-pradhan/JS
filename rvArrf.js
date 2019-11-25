350017da6c5d64e58b94c68f003a136e8266e4dc
ayusha-pradhan <39608243+ayusha-pradhan@users.noreply.github.com>
2019-11-25 08:04:20 +0545

    Delete rvArrf.js

--- rvArrf.js
+++
@@ -1,8 +0,0 @@
-function reverseArray (arr) {
-    var newArr = [];
-    for (var i = 0; i < arr.length; i++) {      
-        newArr[i] = arr[arr.length - i - 1];
-    }   
-    return newArr;
-}
-console.log(reverseArray([1,2,3,4]));
